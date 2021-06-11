0f6921640924d578f436e5c2d949e0661cd12c9e
Jankin-Bai(qiankong) <jankinbai@qq.com>
2021-06-12 01:29:24 +0800

    add head file

--- qiankong/ammeter.c
+++
@@ -1,4 +0,0 @@
-//#include<stdio.h>
-
-#define internalResister 1084 //ohm
-#define  
---
+++ qiankong/ammeter.h
@@ -0,0 +1,9 @@
+//#include<stdio.h>
+/*
+  this object named ammeter
+  I have a old uAammeter，I want to vivid control 
+*/
+
+#define internalResister 1084 //ohm
+#define range 200 //uA
+
